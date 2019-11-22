c26860a88472c9b0a03b9086cc01d6c594552a1b
liukun <kun.liu@winbaoxian.com>
2019-11-22 16:32:52 +0800

    一次更新吧

--- src/views/test/profession.vue
+++ src/views/test/profession.vue
@@ -2,16 +2,25 @@
   <div class="profession-container">
     <bxs-button type="primary" @click="value=true">职业弹窗</bxs-button>
     <bxs-profession-list :columns="columns" v-model="value" ></bxs-profession-list>
+    <button type="button" @click="popPicker">展示</button>
+    <button type="button" @click="handleDate">打开</button>
+    <button type="button" @click="closeDate">关闭</button>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import { Picker, DatetimePicker } from 'bxs-ui-vue'
+  Vue.use(Picker);
   export default {
     name: 'profession',
     components: {},
     data() {
       return {
+        minDate: new Date('1970/01/01'),
+        maxDate: new Date('1970/01/02'),
         value: false,
+        currentDate: '',
         columns: [
           {
             des: '印刷',
@@ -24,12 +33,62 @@
               }]
             }]
           }
-        ]
+        ],
+        citys: [
+          {
+            "key": "beneficiary_relation",
+            "name": "配偶",
+            "value": "2"
+          },
+          {
+            "key": "beneficiary_relation",
+            "name": "父母",
+            "value": "11"
+          },
+          {
+            "key": "beneficiary_relation",
+            "name": "子女",
+            "value": "12"
+          }
+      ],
+        defaultValue:['beneficiary_relation']
       }
     },
     computed: {},
     watch: {},
-    methods: {},
+    methods: {
+      handleDate() {
+        this.$datetime({
+          value: new Date(),
+          type: 'date',
+          confirmButtonColor:'#508cee',
+          minDate: this.minDate,
+          maxDate: this.maxDate,
+          confirm(data) {
+            console.log(data, 'confirm')
+          },
+          change(data) {
+            console.log(data, 'change')
+          },
+        })
+      },
+      closeDate() {
+        this.$datetime.close()
+      },
+      popPicker(){
+        this.$picker({
+          title: 'picker标题',
+          valueKey: 'name',
+          defaultValue: this.defaultValue,
+          value: this.defaultValue,
+          className: 'test',
+          pickerData: this.citys,
+          callback(backInfo) {
+            console.log(backInfo)
+          },
+        })
+      }
+    },
     created() {
     },
     mounted() {
